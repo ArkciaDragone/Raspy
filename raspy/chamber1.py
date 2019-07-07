87c84d226b435a4183054ab38fd24c5c7a6d2d11
Arkcia <Arkcia@126.com>
2019-07-07 18:41:16 +0800

    Added feature: TNT follow
    See escape/tnt

--- raspy/chamber1.py
+++ raspy/chamber1.py
@@ -1,120 +1,137 @@
 import mcpi.minecraft as mmc
-import tools,time
+import tools, time
 import mcpi.block as block
 import mcpi.entity as entity
 import mcpi.vec3 as vec3
-import escape.tnt
-V3 = vec3.Vec3
-mc=tools.start(0)
-import random,math
-setb=mc.setBlock
-setbs=mc.setBlocks
-wall=block.DIAMOND_BLOCK
-deco=block.BEACON
-def east(x,y,z):
-    setbs(x,y-2,z-5,x+20,y-1,z+5,wall) #floor
-    setbs(x,y-1,z-5,x+20,y+5,z-5,wall) #leftwall
-    setbs(x,y-1,z+5,x+20,y+5,z+5,wall) #rightwall
-    #setbs(x,y+5,z-5,x+20,y+5,z+5,wall) #ceiling
-    setbs(x,y+2,z-5,x+20,y+2,z-5,deco) #leftBeacon
-    setbs(x,y+2,z+5,x+20,y+2,z+5,deco) #rightBeacon
-    setbs(x+3,y-1,z-4,x+17,y-1,z+4,10) #lava
-    for i in range(x+3,x+17,3):         #step
-        for j in range(z-4,z+4,3):
-            m=random.randint(0,3)
-            setbs(i+m,y-1,j+m,i+m+1,y-1,j+m,57)
+from escape.tnt import tntfollow
 
-def west(x,y,z):
-    setbs(x,y-2,z-5,x-20,y-1,z+5,wall)
-    setbs(x,y-1,z-5,x-20,y+5,z-5,wall)
-    setbs(x,y-1,z+5,x-20,y+5,z+5,wall)
-    #setbs(x,y+5,z-5,x-20,y+5,z+5,wall)
-    setbs(x,y+2,z-5,x-20,y+2,z-5,deco)
-    setbs(x,y+2,z+5,x-20,y+2,z+5,deco)
-    setbs(x-3,y-1,z-4,x-17,y-1,z+4,10)
-    for i in range(x-3,x-17,-3):
-        for j in range(z-4,z+4,3):
-            m=random.randint(0,3)
-            setbs(i-m,y-1,j+m,i-m-1,y-1,j+m,57)
+if __name__ == '__main__':
+    V3 = vec3.Vec3
+    mc = tools.start(0)
+    import random, math
 
-def south(x,y,z):
-    setbs(x-5,y-2,z,x+5,y-1,z+20,wall)
-    setbs(x-5,y-1,z,x-5,y+5,z+20,wall)
-    setbs(x+5,y-1,z,x+5,y+5,z+20,wall)
-    #setbs(x-5,y+5,z,x+5,y+5,z+20,wall)
-    setbs(x+5,y+2,z,x+5,y+2,z+20,deco)
-    setbs(x-5,y+2,z,x-5,y+2,z+20,deco)
-    setbs(x-4,y-1,z+3,x+4,y-1,z+17,10)
-    for i in range(z+3,z+17,3):
-        for j in range(x-4,x+4,3):
-            m=random.randint(0,3)
-            setbs(j+m,y-1,i+m,j+m,y-1,i+m+1,57)
-
-def north(x,y,z):
-    setbs(x-5,y-2,z,x+5,y-1,z-20,wall)
-    setbs(x-5,y-1,z,x-5,y+5,z-20,wall)
-    setbs(x+5,y-1,z,x+5,y+5,z-20,wall)
-    #setbs(x-5,y+5,z,x+5,y+5,z-20,wall)
-    setbs(x+5,y+2,z,x+5,y+2,z-20,deco)
-    setbs(x-5,y+2,z,x-5,y+2,z-20,deco)
-    setbs(x-4,y-1,z-3,x+4,y-1,z-17,10)
-    for i in range(z-3,z-17,-3):
-        for j in range(x-4,x+4,3):
-            m=random.randint(0,3)
-            setbs(j+m,y-1,i-m,j+m,y-1,i-m-1,57)
+    setb = mc.setBlock
+    setbs = mc.setBlocks
+    wall = block.DIAMOND_BLOCK
+    deco = block.BEACON
 
 
-start=map(int,input('the starting position:').split())
-direction=input('''the starting direction(east='e',north='n',south='s',west='w'):''')
-pId=mc.getPlayerEntityId(map(mc.entity.getName, mc.getPlayerEntityIds()))
-mc.entity.setTilePos(pId,start)
-start=mc.entity.getTilePos(pId)
-x=start.x
-y=start.y
-z=start.z
-setchance=int(input('this task will be set for how many times:'))
+    def east(x, y, z):
+        setbs(x, y - 2, z - 5, x + 20, y - 1, z + 5, wall)  # floor
+        setbs(x, y - 1, z - 5, x + 20, y + 5, z - 5, wall)  # leftwall
+        setbs(x, y - 1, z + 5, x + 20, y + 5, z + 5, wall)  # rightwall
+        # setbs(x,y+5,z-5,x+20,y+5,z+5,wall) #ceiling
+        setbs(x, y + 2, z - 5, x + 20, y + 2, z - 5, deco)  # leftBeacon
+        setbs(x, y + 2, z + 5, x + 20, y + 2, z + 5, deco)  # rightBeacon
+        setbs(x + 3, y - 1, z - 4, x + 17, y - 1, z + 4, 10)  # lava
+        for i in range(x + 3, x + 17, 3):  # step
+            for j in range(z - 4, z + 4, 3):
+                m = random.randint(0, 3)
+                setbs(i + m, y - 1, j + m, i + m + 1, y - 1, j + m, 57)
 
-if direction=='e':
-    east(x,y,z)
-    i=0
-    while i<setchance:
-        while True:
-            if mc.entity.getTilePos(pId).x > x+5:
-                x+=18
-                east(x,y,z)
-                break
-            time.sleep(0.1)
-        i+=1
-elif direction=='n':
-    north(x,y,z)
-    i=0
-    while i<setchance:
-        while True:
-            if mc.entity.getTilePos(pId).z < z-5:
-                z-=18
-                nprth(x,y,z)
-                break
-            time.sleep(0.1)
-        i+=1
-elif direction=='s':
-    south(x,y,z)
-    i=0
-    while i<setchance:
-        while True:
-            if mc.entity.getTilePos(pId).z > z+5:
-                z+=18
-                south(x,y,z)
-                break
-            time.sleep(0.1)
-        i+=1
-elif direction=='w':
-    west(x,y,z)
-    i=0
-    while i<setchance:
-        while True:
-            if mc.entity.getTilePos(pId).x < x-5:
-                x-=18
-                west(x,y,z)
-                break
-            time.sleep(0.1)
-        i+=1
+
+    def west(x, y, z):
+        setbs(x, y - 2, z - 5, x - 20, y - 1, z + 5, wall)
+        setbs(x, y - 1, z - 5, x - 20, y + 5, z - 5, wall)
+        setbs(x, y - 1, z + 5, x - 20, y + 5, z + 5, wall)
+        # setbs(x,y+5,z-5,x-20,y+5,z+5,wall)
+        setbs(x, y + 2, z - 5, x - 20, y + 2, z - 5, deco)
+        setbs(x, y + 2, z + 5, x - 20, y + 2, z + 5, deco)
+        setbs(x - 3, y - 1, z - 4, x - 17, y - 1, z + 4, 10)
+        for i in range(x - 3, x - 17, -3):
+            for j in range(z - 4, z + 4, 3):
+                m = random.randint(0, 3)
+                setbs(i - m, y - 1, j + m, i - m - 1, y - 1, j + m, 57)
+
+
+    def south(x, y, z):
+        setbs(x - 5, y - 2, z, x + 5, y - 1, z + 20, wall)
+        setbs(x - 5, y - 1, z, x - 5, y + 5, z + 20, wall)
+        setbs(x + 5, y - 1, z, x + 5, y + 5, z + 20, wall)
+        # setbs(x-5,y+5,z,x+5,y+5,z+20,wall)
+        setbs(x + 5, y + 2, z, x + 5, y + 2, z + 20, deco)
+        setbs(x - 5, y + 2, z, x - 5, y + 2, z + 20, deco)
+        setbs(x - 4, y - 1, z + 3, x + 4, y - 1, z + 17, 10)
+        for i in range(z + 3, z + 17, 3):
+            for j in range(x - 4, x + 4, 3):
+                m = random.randint(0, 3)
+                setbs(j + m, y - 1, i + m, j + m, y - 1, i + m + 1, 57)
+
+
+    def north(x, y, z):
+        setbs(x - 5, y - 2, z, x + 5, y - 1, z - 20, wall)
+        setbs(x - 5, y - 1, z, x - 5, y + 5, z - 20, wall)
+        setbs(x + 5, y - 1, z, x + 5, y + 5, z - 20, wall)
+        # setbs(x-5,y+5,z,x+5,y+5,z-20,wall)
+        setbs(x + 5, y + 2, z, x + 5, y + 2, z - 20, deco)
+        setbs(x - 5, y + 2, z, x - 5, y + 2, z - 20, deco)
+        setbs(x - 4, y - 1, z - 3, x + 4, y - 1, z - 17, 10)
+        for i in range(z - 3, z - 17, -3):
+            for j in range(x - 4, x + 4, 3):
+                m = random.randint(0, 3)
+                setbs(j + m, y - 1, i - m, j + m, y - 1, i - m - 1, 57)
+
+
+    start = map(int, input('the starting position:').split())
+    direction = input('''the starting direction(east='e',north='n',south='s',west='w'):''')
+    pId = mc.getPlayerEntityId(map(mc.entity.getName, mc.getPlayerEntityIds()))
+    mc.entity.setTilePos(pId, start)
+    start = mc.entity.getTilePos(pId)
+    x = start.x
+    y = start.y
+    z = start.z
+    setchance = int(input('this task will be set for how many times:'))
+    from multiprocessing import Process
+    import os
+    import escape.tnt
+
+    print("Initializing TNT follow.")
+    p = Process(target=tntfollow, args=(0, 0))
+    if direction == 'e':
+        east(x, y, z)
+        p.start()
+        i = 0
+        while i < setchance:
+            while True:
+                if mc.entity.getTilePos(pId).x > x + 5:
+                    x += 18
+                    east(x, y, z)
+                    break
+                time.sleep(0.1)
+            i += 1
+    elif direction == 'n':
+        north(x, y, z)
+        p.start()
+        i = 0
+        while i < setchance:
+            while True:
+                if mc.entity.getTilePos(pId).z < z - 5:
+                    z -= 18
+                    north(x, y, z)
+                    break
+                time.sleep(0.1)
+            i += 1
+    elif direction == 's':
+        south(x, y, z)
+        p.start()
+        i = 0
+        while i < setchance:
+            while True:
+                if mc.entity.getTilePos(pId).z > z + 5:
+                    z += 18
+                    south(x, y, z)
+                    break
+                time.sleep(0.1)
+            i += 1
+    elif direction == 'w':
+        west(x, y, z)
+        p.start()
+        i = 0
+        while i < setchance:
+            while True:
+                if mc.entity.getTilePos(pId).x < x - 5:
+                    x -= 18
+                    west(x, y, z)
+                    break
+                time.sleep(0.1)
+            i += 1
--- raspy/escape/tnt.py
+++ raspy/escape/tnt.py
@@ -1,23 +1,25 @@
-import tools,time
-#输入TNT数量，0表示无限
-def tntfollow(number):
-    mc=tools.start(0)
-    setb=mc.setBlock
-    setbs=mc.setBlocks
-    spawn=mc.spawnEntity
-    pId=mc.getPlayerEntityId(map(mc.entity.getName, mc.getPlayerEntityIds()))
-    start=mc.entity.getTilePos(pId)
-    if number==0:
+import time, os
+import tools
+import mcpi.entity as entity
+
+
+# 输入TNT数量，0表示无限
+def tntfollow(server_port=0, number=0):
+    """Generate TNT to follow the first player (server_port, tnt count)"""
+    print("TNT follow started on {}".format(os.getpid()))
+    mc = tools.start(server_port)
+    spawn = mc.spawnEntity
+    pId = mc.getPlayerEntityIds()[0]
+    pos = mc.entity.getTilePos(pId)
+    if number == 0:
         while True:
-            spawn(start.x,start.y+7,start.z,20)
-            start=mc.entity.getTilePos(pId)
+            spawn(pos.up(7), entity.PRIMED_TNT.id)
+            pos = mc.entity.getTilePos(pId)
             time.sleep(1)
     else:
-        i=0
-        while i<number:
-            spawn(start.x,start.y+7,start.z,20)
-            start=mc.entity.getTilePos(pId)
+        i = 0
+        while i < number:
+            spawn(pos.up(7), entity.PRIMED_TNT.id)
+            pos = mc.entity.getTilePos(pId)
             time.sleep(1)
-            i+=1
-    
-            
+            i += 1
