e2e757b07863b12c9e1dbf4046e6da10eca9ea47
Fannie <1810108608@pku.edu.cn>
2019-07-29 13:53:19 +0800

    spiral stair

--- raspy/escape/spiralstair.py
+++ raspy/escape/spiralstair.py
@@ -5,19 +5,23 @@ import tools, time
 import mcpi.block as block
 import mcpi.entity as entity
 import mcpi.vec3 as vec3
-from escape.tnt import tntfollow
+from escape.interface import *
 
 if __name__ == '__main__':
     V3 = vec3.Vec3
     mc = tools.start(0)
 
-
-    setb = mc.setBlock
-    setbs = mc.setBlocks
-
-
-
-    def heart(x, y, z):
+class SpiralStair(Level):
+    @staticmethod
+    def exitWin(entrance: Window):
+        return entrance
+    
+    def _construct(self):
+        setb = self.mc.setBlock
+        setbs = self.mc.setBlocks
+        x, y, z = self.entWin.middle
+        setbs(x+2,y-1,z-8,x+4,y+10,z+8,0)
+        setbs(x-2,y-1,z-8,x-4,y+10,z+8,0)
         setb(x, y, z, 42)
         setb(x + 1, y, z, 42)
         setb(x - 1, y, z, 42)
@@ -60,33 +64,23 @@ if __name__ == '__main__':
         setb(x - 7, y + 10, z + 2, 42)
         setb(x - 5, y + 10, z + 4, 41)
         setb(x - 3, y + 11, z + 5, 42)
+        setb(x, y+11, z+2, 42)
+        setb(x + 1, y+11, z+2, 42)
+        setb(x - 1, y+11, z+2, 42)
+        setb(x, y+11, z + 3, 42)
+        setb(x, y+11, z+1, 42)
+        setb(x + 2, y+11, z+2, 41)
+        setb(x - 2, y+11, z+2, 41)
+        setb(x + 1, y+11, z + 3, 41)
+        setb(x - 1, y+11, z + 3, 41)
+        setb(x + 1, y+11, z+1, 41)
+        setb(x - 1, y+11, z+1, 41)
+        setb(x, y+11, z, 41)
+        setb(x, y+11, z + 4, 41)
+        entrance.middle.y = entrance.middle.y+10
+    def _loop(self):
+        """静态关卡"""
+        pass
 
-
-        
-
-    start = map(int, input('the starting position:').split())
-    pId = mc.getPlayerEntityId(map(mc.entity.getName, mc.getPlayerEntityIds()))
-    mc.entity.setTilePos(pId, start)
-    start = mc.entity.getTilePos(pId)
-    x = start.x
-    y = start.y
-    z = start.z
-    setchance = int(input('this task will be set for how many times:'))
-
-    print("...")
-    heart(x, y, z)
-    i = 0
-    while i < setchance:
-        while True:
-            if mc.entity.getTilePos(pId).y > y + 10:
-                y += 11
-                heart(x, y, z)
-                break
-            time.sleep(0.1)
-        i += 1
-    k=0
-
-    """while k>=0:
-        setbs(x+8,y,z+8,x-8,y+k,z-8,0)
-        time.sleep(3)
-    k+=1"""
+    def _cleanup(self):
+        pass
